cf984cb47b754e5282dd00586c8c066c8bbaaa08
Rafael Gabriel <git@rafaelgabriel.dev>
2025-04-17 05:43:31 -0300

    Fix path to steamcmd in launch script for server and mod updates

--- lauch_server.sh
+++ lauch_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "atualizando o servidor Project Zomboid..."
-steamcmd +login anonymous +app_update 380870 validate +quit
+../Steam/steamcmd.sh +login anonymous +app_update 380870 validate +quit
 
 echo "atualizando os mods do servidor Project Zomboid..."
 MODS=$(grep "WorkshopItems=" "Server/${SERVER_NAME}.ini" | cut -d'=' -f2)
@@ -9,7 +9,7 @@ MODS=$(grep "WorkshopItems=" "Server/${SERVER_NAME}.ini" | cut -d'=' -f2)
 IFS=';' read -ra MOD_ARRAY <<< "$MODS"
 for mod in "${MOD_ARRAY[@]}"; do
     echo "Verificando atualização para o mod: $mod"
-    steamcmd +login anonymous +workshop_download_item 108600 $mod +quit
+    ../Steam/steamcmd.sh +login anonymous +workshop_download_item 108600 $mod +quit
 done
 
 /opt/project_zomboid/Server/start-server.sh -servername ${SERVER_NAME} -adminusername ${ADMIN_USERNAME} -adminpassword ${ADMIN_PASSWORD} -Xmx${MAX_MEMORY} -Xms${MIN_MEMORY}
